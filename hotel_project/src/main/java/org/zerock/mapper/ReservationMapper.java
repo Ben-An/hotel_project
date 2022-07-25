652eee394d14aa1688d61a8a1ca918b5459c7a91
Ben-An <byungjinan13@naver.com>
2022-07-25 17:48:54 +0900

    lagged

---
+++ hotel_project/src/main/java/org/zerock/mapper/ReservationMapper.java
@@ -0,0 +1,12 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.zerock.domain.ReservationVO;
+
+public interface ReservationMapper {
+	
+	public ReservationVO reservationList(int roomno);
+	
+	
+}
---
+++ hotel_project/src/main/java/org/zerock/service/ReservationService.java
@@ -0,0 +1,11 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.zerock.domain.ReservationVO;
+
+public interface ReservationService {
+
+	public ReservationVO ReseravationList(int roomno);
+	
+}
