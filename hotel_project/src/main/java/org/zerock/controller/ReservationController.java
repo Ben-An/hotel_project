97d40f5d25e0d0bc58e522ad434efa469780b389
ByungAn <103394900+ByungAn@users.noreply.github.com>
2022-07-27 08:31:44 +0900

    wokring on kakaopay and insert reservation submission

--- hotel_project/src/main/java/org/zerock/controller/ReservationController.java
+++ hotel_project/src/main/java/org/zerock/controller/ReservationController.java
@@ -9,15 +9,20 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.stream.Collectors;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.zerock.domain.PayRservationVO;
 import org.zerock.service.ReservationService;
 
 import lombok.AllArgsConstructor;
@@ -48,13 +53,17 @@ public class ReservationController {
 	
 	
 
-	@PostMapping("/reservePayment")
+	
 	@ResponseBody
-	public void reservePay()
+	@PostMapping(value="/reservePayment",consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> reservePay(@RequestBody PayRservationVO payVO)
 	{
+		log.info(payVO);
 		
+		int result = service.payRegister(payVO);
 		
-		
+		return result ==1 ? new ResponseEntity<>("success",HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	
@@ -69,32 +78,26 @@ public class ReservationController {
 			connection.setRequestProperty("Authorization", "KakaoAK e9a69f79dbf4f0106b16ac148df3b7a1");
 			connection.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 			connection.setDoOutput(true);
-			String parameters="cid=TC0ONETIME&partner_order_id=partner_order_id&partner_user_id=partner_user_id&item_name=초코파이&quantity=1&total_amount=2200&vat_amount=200&tax_free_amount=0&approval_url=https://developers.kakao.com/success&fail_url=https://developers.kakao.com/fail&cancel_url=https://developers.kakao.com/cancel";
+			String parameters="cid=TC0ONETIME&partner_order_id=partner_order_id&partner_user_id=partner_user_id&item_name=초코파이&quantity=1&total_amount=2200&vat_amount=200&tax_free_amount=0&approval_url=https://localhost:8080/hotel/kakaoPay.cls&fail_url=https://localhost:8080/fail&cancel_url=https://localhost:8080/cancel";
 			OutputStream given = connection.getOutputStream();
 			DataOutputStream dataGiven = new DataOutputStream(given);
 			dataGiven.writeBytes(parameters);
 			dataGiven.close();
-			
-			log.info(parameters);
-			
-			
-			
+
 			int result = connection.getResponseCode();
 			
 			InputStream inputstream;
-			if(result ==200) {
+			if(result == 200) {
 				inputstream = connection.getInputStream();
 				
 			}else {
 				inputstream = connection.getErrorStream();
 				
 			}
-			
 			InputStreamReader inputStreamReader = new InputStreamReader(inputstream);
 			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-			
-			
-			
+			log.info(bufferedReader.readLine());
+			/* return bufferedReader.readLine(); */
 			
 			return bufferedReader.readLine();
 		} catch (MalformedURLException e) {
@@ -102,9 +105,9 @@ public class ReservationController {
 		}catch (IOException e) {
 			e.printStackTrace();
 		}
-		return "{\"result\" : \"NO\"}";
+			
 		
-		
+		return "{\"result\":\"NO\"}";
 	}
 	
 	
--- hotel_project/src/main/java/org/zerock/service/ReservationService.java
+++ hotel_project/src/main/java/org/zerock/service/ReservationService.java
@@ -2,10 +2,13 @@ package org.zerock.service;
 
 import java.util.List;
 
+import org.zerock.domain.PayRservationVO;
 import org.zerock.domain.ReservationVO;
 
 public interface ReservationService {
 
 	public ReservationVO ReseravationList(int roomno);
+	
+	public int payRegister(PayRservationVO payVO);
 	
 }
